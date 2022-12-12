e22eaea56532d11053bbc2df0d5819395aec8342
Aivaras Jurgutis <you@example.com>
2022-12-12 15:31:19 +0200

    Mano pirmasis komitas

---
+++ js/mano-skriptas.js
@@ -0,0 +1,18 @@
+let a = 1;
+let result = ('Kintamojo a reikšmė:') + a;
+console.log(result);
+a = 2 ;
+console.log(result);
+let c = 'Mano vardas';
+let b = 'Aivaras Jurgutis';
+console.log(c,b);
+
+
+
+
+
+
+
+
+
+
