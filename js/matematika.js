6db1c04873c3365caa75c2e7058889bc7bf55153
Aivaras Jurgutis <you@example.com>
2022-12-15 23:08:24 +0200

    JSS uzduociu atlikimas su masyvais

---
+++ js/matematika.js
@@ -0,0 +1,28 @@
+function add(arr1,arr2) {
+    arr1[1] + arr2[6], arr1[1] + arr2[7], arr1[1] + arr2[8], arr1[1] + arr2[9], arr1[1] + arr2[10],
+    arr1[2] + arr2[6], arr1[2] + arr2[7], arr1[2] + arr2[8], arr1[2] + arr2[9], arr1[2] + arr2[10],
+    arr1[3] + arr2[6], arr1[3] + arr2[7], arr1[3] + arr2[8], arr1[3] + arr2[9], arr1[3] + arr2[10],
+    arr1[4] + arr2[6], arr1[4] + arr2[7], arr1[4] + arr2[8], arr1[4] + arr2[9], arr1[4] + arr2[10],
+    arr1[5] + arr2[6], arr1[5] + arr2[7], arr1[5] + arr2[8], arr1[5] + arr2[9], arr1[5] + arr2[10]
+
+}
+function sub() {
+
+}
+function div() {
+
+}
+function mult () {
+
+}
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [6, 7, 8, 9, 10];
+
+for (let i = 0; i < arr1.length; i++) {
+    //console.log(arr1[i] + arr2[i]);
+    //..
+    for (let j = 0; j < arr2.length; j++) {
+        // console.log(arr2[i] + [j]);
+        //..
+    }
+}
